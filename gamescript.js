66bc1a6b5ced2ebac18e51dfe43808b2ac768831
M.Ehsan <124778596+ehsan-saf@users.noreply.github.com>
2023-04-05 15:16:42 +0330

    Write code to randomly return an option

--- gamescript.js
+++ gamescript.js
@@ -1,7 +1,12 @@
 // A list of possible game options
+const gameOptions = ['Rock', 'Paper', 'Scissors'];
 
 // Create getComputerChoice function
+function getComputerChoice(){
 
  // Randomly choose one of the options and store it in a variable
+ let randomChoice = gameOptions[Math.floor(Math.random() * 3)];
 
  // Return the variable 
+ return randomChoice;
+}
